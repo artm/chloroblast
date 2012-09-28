17499e23cbb7c27a49c39b42a2a26958c03a31bb
Artem Baguinski <artm@v2.nl>
2012-09-28 11:06:05 +0200

    switching to boilerplatish layout

--- chloroplast.rb
+++ chloroplast.rb
@@ -12,3 +12,15 @@ set :haml, :format => :html5
 get '/' do
   haml :index
 end
+
+helpers do
+  def title ; 'Chloroplast' end
+  def page_classes ; 'page' end
+  def jquery_version ; '1.8.2' end
+  def jquery_cdn_url
+    "//ajax.googleapis.com/ajax/libs/jquery/#{jquery_version}/jquery.min.js"
+  end
+  def jquery_local_url ; "js/libs/jquery-#{jquery_version}.min.js" end
+  def modernizr_url ; 'js/libs/modernizr.custom.21789.js' end
+end
+
